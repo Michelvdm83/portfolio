fdb35dfc1a50264e29471e7c80f61c6ef063da92
Michelvdm83 <michelvdm@gmail.com>
2024-08-07 19:24:01 +0200

    end of day update
    added working test of different sprite animation.
    added "board" to main

--- script/functions.js
+++ script/functions.js
@@ -3,159 +3,190 @@ const sora2Div = document.getElementById("sora2");
 const movers = document.getElementById("carousel");
 
 function firstLoad() {
-  movers.childNodes.forEach((navButton) => {
-    let newNode = navButton.cloneNode(true);
-    newNode.id = navButton.id + "1";
-    movers.appendChild(newNode);
-  });
-  soraDiv.style.animationDelay = "-58.5s";
-  sora2Div.style.animationDelay = "-28.5s";
+    movers.childNodes.forEach((navButton) => {
+        let newNode = navButton.cloneNode(true);
+        newNode.id = navButton.id + "1";
+        movers.appendChild(newNode);
+    });
+    soraDiv.style.animationDelay = "-58.5s";
+    sora2Div.style.animationDelay = "-28.5s";
 
-  const headers = document.getElementById("top-header");
-  headers.childNodes.forEach((sprite) => {
-    if (sprite.className == "sora") {
-      let newSprite = sprite.cloneNode(true);
-      newSprite.id = sprite.id + "-contact";
-      let originalDelay = sprite.style.animationDelay;
-      let secondsDelay = new Number(
-        originalDelay.substring(0, originalDelay.length - 1)
-      );
-      let newDelay = secondsDelay + 10;
-      if (newDelay > 0) {
-        newDelay -= 60;
-      }
-      newSprite.style.animationDelay = newDelay + "s";
-      headers.appendChild(newSprite);
-    }
-  });
-
-  movers.childNodes.forEach((d) => {
-    if (d.id == "about-button" || d.id == "about-button1") {
-      d.onclick = function (event) {
-        (d.id == "about-button" ? soraDiv : sora2Div).animate(
-          [
-            { backgroundPosition: "100% 0" },
-            { backgroundPosition: "100% -581px" },
-          ],
-          {
-            duration: 1000,
-            easing: "steps(7)",
-          }
-        );
-
-        event.target.animate(
-          [
-            { transform: "translate(0, 0)" },
-            { transform: "translate(0, -40%" },
-            { transform: "translate(0, 0)" },
-          ],
-          {
-            duration: 1000,
-            delay: 200,
-            easing: "ease-in-out",
-          }
-        );
-
-        setTimeout(goto("about"), 1000);
-      };
-    }
-
-    if (d.id == "contact-button" || d.id == "contact-button1") {
-      const soraContact1 = document.getElementById("sora-contact");
-      const soraContact2 = document.getElementById("sora2-contact");
-      d.onclick = function (event) {
-        (d.id == "contact-button" ? soraContact1 : soraContact2).animate(
-          [
-            { backgroundPosition: "100% 0" },
-            { backgroundPosition: "100% -581px" },
-          ],
-          {
-            duration: 1000,
-            easing: "steps(7)",
-          }
-        );
-
-        event.target.animate(
-          [
-            { transform: "translate(0, 0)" },
-            { transform: "translate(0, -40%" },
-            { transform: "translate(0, 0)" },
-          ],
-          {
-            duration: 1000,
-            delay: 200,
-            easing: "ease-in-out",
-          }
-        );
-        setTimeout(goto("contact"), 1000);
-      };
-    }
-
-    if (d.id === "links-button" || d.id === "links-button1") {
-      d.onclick = () => goto("links");
-    }
-  });
-
-  movers.onmouseenter = function (e) {
     const headers = document.getElementById("top-header");
     headers.childNodes.forEach((sprite) => {
-      if (sprite.className == "sora") {
-        sprite.style.animationPlayState = "paused";
-      }
+        if (sprite.className == "sora") {
+            let newSprite = sprite.cloneNode(true);
+            newSprite.id = sprite.id + "-contact";
+            let originalDelay = sprite.style.animationDelay;
+            let secondsDelay = new Number(
+                originalDelay.substring(0, originalDelay.length - 1)
+            );
+            let newDelay = secondsDelay + 10;
+            if (newDelay > 0) {
+                newDelay -= 60;
+            }
+            newSprite.style.animationDelay = newDelay + "s";
+            headers.appendChild(newSprite);
+        }
     });
-    // soraDiv.style.animationPlayState = "paused";
-    // sora2Div.style.animationPlayState = "paused";
-    const x = e.clientX;
-    const soraX = soraDiv.getBoundingClientRect().x;
-  };
-  movers.onmouseleave = function () {
-    const headers = document.getElementById("top-header");
-    headers.childNodes.forEach((sprite) => {
-      if (sprite.className == "sora") {
-        sprite.style.animationPlayState = "running";
-      }
-    });
-    soraDiv.style.animationPlayState = "running";
-    sora2Div.style.animationPlayState = "running";
-  };
 
-  movers.ontouchend = function () {
-    const headers = document.getElementById("top-header");
-    headers.childNodes.forEach((sprite) => {
-      if (sprite.className == "sora") {
-        console.log("check");
-        sprite.style.animationPlayState = "running";
-      }
-    });
-    soraDiv.style.animationPlayState = "running";
-    sora2Div.style.animationPlayState = "running";
+    movers.childNodes.forEach((d) => {
+        if (d.id == "home-container") {
+            const testSora = document.getElementById("sora3");
+            d.onclick = function (event) {
+                testSora.animate(
+                    [
+                        { backgroundPosition: "100% 0" },
+                        { backgroundPosition: "100% -581px" },
+                    ],
+                    {
+                        duration: 1000,
+                        easing: "steps(7)",
+                    }
+                );
 
-    movers.style.animationPlayState = "running";
-  };
+                event.target.animate(
+                    [
+                        { transform: "translate(0, 0)" },
+                        { transform: "translate(0, -40%" },
+                        { transform: "translate(0, 0)" },
+                    ],
+                    {
+                        duration: 1000,
+                        delay: 200,
+                        easing: "ease-in-out",
+                    }
+                );
+            };
+        }
+        if (d.id == "about-button" || d.id == "about-button1") {
+            d.onclick = function (event) {
+                (d.id == "about-button" ? soraDiv : sora2Div).animate(
+                    [
+                        { backgroundPosition: "100% 0" },
+                        { backgroundPosition: "100% -581px" },
+                    ],
+                    {
+                        duration: 1000,
+                        easing: "steps(7)",
+                    }
+                );
+
+                event.target.animate(
+                    [
+                        { transform: "translate(0, 0)" },
+                        { transform: "translate(0, -40%" },
+                        { transform: "translate(0, 0)" },
+                    ],
+                    {
+                        duration: 1000,
+                        delay: 200,
+                        easing: "ease-in-out",
+                    }
+                );
+
+                setTimeout(goto("about"), 1000);
+            };
+        }
+
+        if (d.id == "contact-button" || d.id == "contact-button1") {
+            const soraContact1 = document.getElementById("sora-contact");
+            const soraContact2 = document.getElementById("sora2-contact");
+            d.onclick = function (event) {
+                (d.id == "contact-button"
+                    ? soraContact1
+                    : soraContact2
+                ).animate(
+                    [
+                        { backgroundPosition: "100% 0" },
+                        { backgroundPosition: "100% -581px" },
+                    ],
+                    {
+                        duration: 1000,
+                        easing: "steps(7)",
+                    }
+                );
+
+                event.target.animate(
+                    [
+                        { transform: "translate(0, 0)" },
+                        { transform: "translate(0, -40%" },
+                        { transform: "translate(0, 0)" },
+                    ],
+                    {
+                        duration: 1000,
+                        delay: 200,
+                        easing: "ease-in-out",
+                    }
+                );
+                setTimeout(goto("contact"), 1000);
+            };
+        }
+
+        if (d.id === "links-button" || d.id === "links-button1") {
+            d.onclick = () => goto("links");
+        }
+    });
+
+    movers.onmouseenter = function (e) {
+        const headers = document.getElementById("top-header");
+        headers.childNodes.forEach((sprite) => {
+            if (sprite.className == "sora") {
+                sprite.style.animationPlayState = "paused";
+            }
+        });
+        // soraDiv.style.animationPlayState = "paused";
+        // sora2Div.style.animationPlayState = "paused";
+        const x = e.clientX;
+        const soraX = soraDiv.getBoundingClientRect().x;
+    };
+    movers.onmouseleave = function () {
+        const headers = document.getElementById("top-header");
+        headers.childNodes.forEach((sprite) => {
+            if (sprite.className == "sora") {
+                sprite.style.animationPlayState = "running";
+            }
+        });
+        soraDiv.style.animationPlayState = "running";
+        sora2Div.style.animationPlayState = "running";
+    };
+
+    movers.ontouchend = function () {
+        const headers = document.getElementById("top-header");
+        headers.childNodes.forEach((sprite) => {
+            if (sprite.className == "sora") {
+                console.log("check");
+                sprite.style.animationPlayState = "running";
+            }
+        });
+        soraDiv.style.animationPlayState = "running";
+        sora2Div.style.animationPlayState = "running";
+
+        movers.style.animationPlayState = "running";
+    };
 }
 
 function createAlternateHeader() {
-  let newHeader = document.createElement("header");
-  newHeader.classList.add("static-container");
-  movers.childNodes.forEach((child) => {
-    if (child.id && !child.id.includes("1")) {
-      const newCopy = child.cloneNode(true);
-      newHeader.appendChild(newCopy);
+    let newHeader = document.createElement("header");
+    newHeader.classList.add("static-container");
+    movers.childNodes.forEach((child) => {
+        if (child.id && !child.id.includes("1")) {
+            const newCopy = child.cloneNode(true);
+            newHeader.appendChild(newCopy);
 
-      if (newCopy.id === "about-button") {
-        newCopy.onclick = () => goto("about");
-      }
+            if (newCopy.id === "about-button") {
+                newCopy.onclick = () => goto("about");
+            }
 
-      if (newCopy.id === "contact-button") {
-        newCopy.onclick = () => goto("contact");
-      }
+            if (newCopy.id === "contact-button") {
+                newCopy.onclick = () => goto("contact");
+            }
 
-      if (newCopy.id === "links-button") {
-        newCopy.onclick = () => goto("links");
-      }
-    }
-  });
-  return newHeader;
+            if (newCopy.id === "links-button") {
+                newCopy.onclick = () => goto("links");
+            }
+        }
+    });
+    return newHeader;
 }
 
 let animationActive = true;
@@ -163,18 +194,18 @@ let animatedHeader = document.getElementById("top-header");
 let alternateHeader = createAlternateHeader();
 
 function toggle() {
-  const toggle = document.getElementById("animation-toggle");
-  let slider = document.getElementById("animation-slider");
-  if (animationActive) {
-    toggle.classList.remove("animations");
-    toggle.classList.add("no-animations");
-    slider.innerText = "off";
-    document.body.replaceChild(alternateHeader, animatedHeader);
-  } else {
-    toggle.classList.remove("no-animations");
-    toggle.classList.add("animations");
-    slider.innerText = "on";
-    document.body.replaceChild(animatedHeader, alternateHeader);
-  }
-  animationActive = !animationActive;
+    const toggle = document.getElementById("animation-toggle");
+    let slider = document.getElementById("animation-slider");
+    if (animationActive) {
+        toggle.classList.remove("animations");
+        toggle.classList.add("no-animations");
+        slider.innerText = "off";
+        document.body.replaceChild(alternateHeader, animatedHeader);
+    } else {
+        toggle.classList.remove("no-animations");
+        toggle.classList.add("animations");
+        slider.innerText = "on";
+        document.body.replaceChild(animatedHeader, alternateHeader);
+    }
+    animationActive = !animationActive;
 }
--- script/pages.js
+++ script/pages.js
@@ -7,146 +7,160 @@ const contactPage = createContactPage();
 let activePage;
 
 function createAboutPage() {
-  let aboutDiv = document.createElement("article");
-  let aboutTitle = document.createElement("h1");
-  aboutTitle.textContent = "About";
+    let aboutDiv = document.createElement("div");
+    let aboutTitle = document.createElement("h1");
+    aboutTitle.textContent = "About";
 
-  let aboutContext = document.createElement("div");
-  aboutContext.innerText =
-    "Hoi,\n\n Mijn naam is Michel.\n" +
-    "Op het moment ben ik een student bij ITVitae.\n" +
-    "Ik volg de studie tot Java programmeur.\n" +
-    "Hier leren wij, naast 'gewoon' Java, ook het gebruik van het framework Spring Boot voor back-end applicaties.\n" +
-    "Tevens leren wij ook Javascript/REACT voor front-end.";
+    let aboutContext = document.createElement("div");
+    aboutContext.innerText =
+        "Hoi,\n\n Mijn naam is Michel.\n" +
+        "Op het moment ben ik een student bij ITVitae.\n" +
+        "Ik volg de studie tot Java programmeur.\n" +
+        "Hier leren wij, naast 'gewoon' Java, ook het gebruik van het framework Spring Boot voor back-end applicaties.\n" +
+        "Tevens leren wij ook Javascript/REACT voor front-end.";
 
-  aboutDiv.className = "text-field";
-  aboutDiv.id = "about";
-  aboutDiv.appendChild(aboutTitle);
-  aboutDiv.appendChild(aboutContext);
+    aboutDiv.className = "text-field";
+    aboutDiv.id = "about";
+    aboutDiv.appendChild(aboutTitle);
+    aboutDiv.appendChild(aboutContext);
 
-  return aboutDiv;
+    return aboutDiv;
 }
 
 function createLinksPage() {
-  let linksDiv = document.createElement("article");
-  linksDiv.id = "links";
-  linksDiv.className = "text-field";
+    let linksDiv = document.createElement("div");
+    linksDiv.id = "links";
+    linksDiv.className = "text-field";
 
-  let linksTitle = document.createElement("h1");
-  linksTitle.textContent = "Links";
+    let linksTitle = document.createElement("h1");
+    linksTitle.textContent = "Links";
 
-  let githubContext = document.createElement("div");
+    let githubContext = document.createElement("div");
 
-  const githubText = document.createTextNode("github: ");
-  githubContext.appendChild(githubText);
+    const githubText = document.createTextNode("github: ");
+    githubContext.appendChild(githubText);
 
-  let githubLink = document.createElement("a");
-  githubLink.href = "https://github.com/Michelvdm83";
-  githubLink.target = "blank";
-  githubLink.innerText = "Michelvdm83";
-  githubContext.appendChild(githubLink);
-  linksDiv.appendChild(githubContext);
+    let githubLink = document.createElement("a");
+    githubLink.href = "https://github.com/Michelvdm83";
+    githubLink.target = "blank";
+    githubLink.innerText = "Michelvdm83";
+    githubContext.appendChild(githubLink);
+    linksDiv.appendChild(githubContext);
 
-  let linkedinContext = document.createElement("div");
+    let linkedinContext = document.createElement("div");
 
-  const linkedinText = document.createTextNode("linkedin: ");
-  linkedinContext.appendChild(linkedinText);
+    const linkedinText = document.createTextNode("linkedin: ");
+    linkedinContext.appendChild(linkedinText);
 
-  let linkedinLink = document.createElement("a");
-  linkedinLink.href =
-    "https://www.linkedin.com/in/michel-van-der-mark-083314105/";
-  linkedinLink.target = "blank";
-  linkedinLink.innerText = "Michel van der Mark";
-  linkedinContext.appendChild(linkedinLink);
-  linksDiv.appendChild(linkedinContext);
+    let linkedinLink = document.createElement("a");
+    linkedinLink.href =
+        "https://www.linkedin.com/in/michel-van-der-mark-083314105/";
+    linkedinLink.target = "blank";
+    linkedinLink.innerText = "Michel van der Mark";
+    linkedinContext.appendChild(linkedinLink);
+    linksDiv.appendChild(linkedinContext);
 
-  return linksDiv;
+    return linksDiv;
 }
 
 function createContactPage() {
-  let contactDiv = document.createElement("article");
-  contactDiv.id = "contact";
-  contactDiv.className = "text-field";
+    let contactDiv = document.createElement("div");
+    contactDiv.id = "contact";
+    contactDiv.className = "text-field";
 
-  let contactTitle = document.createElement("h1");
-  contactTitle.textContent = "Contact";
+    let contactTitle = document.createElement("h1");
+    contactTitle.textContent = "Contact";
 
-  let mailContext = document.createElement("div");
+    let mailContext = document.createElement("div");
 
-  const mailText = document.createTextNode("mail: ");
-  mailContext.appendChild(mailText);
+    const mailText = document.createTextNode("mail: ");
+    mailContext.appendChild(mailText);
 
-  let emailContact = document.createElement("a");
-  const emailaddress = document.createTextNode("michelvdm@gmail.com");
-  emailContact.href = "mailto:michelvdm@gmail.com";
-  emailContact.appendChild(emailaddress);
-  mailContext.appendChild(emailContact);
+    let emailContact = document.createElement("a");
+    const emailaddress = document.createTextNode("michelvdm@gmail.com");
+    emailContact.href = "mailto:michelvdm@gmail.com";
+    emailContact.appendChild(emailaddress);
+    mailContext.appendChild(emailContact);
 
-  contactDiv.appendChild(contactTitle);
-  contactDiv.appendChild(mailContext);
+    contactDiv.appendChild(contactTitle);
+    contactDiv.appendChild(mailContext);
 
-  return contactDiv;
+    return contactDiv;
+}
+
+function setActive(pageName) {
+    let mainGrid = document.getElementById("mainContent");
+    mainGrid.childNodes.forEach((gridItem) => {
+        if (gridItem.classList && gridItem.classList.contains("grid-item")) {
+            if (pageName === "about") {
+                gridItem.hidden = true;
+            } else {
+                gridItem.hidden = false;
+            }
+        }
+    });
 }
 
 function goto(pageName) {
-  if (activePage === pageName) {
-    return;
-  }
+    if (activePage === pageName) {
+        return;
+    }
 
-  let page;
-  switch (pageName) {
-    case "about":
-      page = aboutPage;
-      break;
-    case "contact":
-      page = contactPage;
-      break;
-    case "links":
-      page = linksPage;
-      break;
-    default:
-      return;
-  }
-  if (activePage != null) {
-    emptyMain(activePage);
-  }
-  activePage = pageName;
-  enterMain(page);
+    let page;
+    switch (pageName) {
+        case "about":
+            page = aboutPage;
+            break;
+        case "contact":
+            page = contactPage;
+            break;
+        case "links":
+            page = linksPage;
+            break;
+        default:
+            return;
+    }
+    setActive(pageName);
+    if (activePage != null) {
+        emptyMain(activePage);
+    }
+    activePage = pageName;
+    enterMain(page);
 }
 
 function removeFromMain(page) {
-  if (activePage === page.id || page.parentElement !== mainBody) {
-    return;
-  }
-  mainBody.removeChild(page);
+    if (activePage === page.id || page.parentElement !== mainBody) {
+        return;
+    }
+    mainBody.removeChild(page);
 }
 
 function emptyMain(currentPage) {
-  let pageToRemove = document.getElementById(`${currentPage}`);
+    let pageToRemove = document.getElementById(`${currentPage}`);
 
-  pageToRemove.animate(
-    [
-      { transform: "translate(0, 0)", opacity: 1 },
-      { transform: "translate(50vw, 0)", opacity: 0 },
-    ],
-    {
-      duration: 2000,
-      fill: "forwards",
-    }
-  );
-  setTimeout(() => removeFromMain(pageToRemove), 2000);
+    pageToRemove.animate(
+        [
+            { transform: "translate(0, 0)", opacity: 1 },
+            { transform: "translate(50vw, 0)", opacity: 0 },
+        ],
+        {
+            duration: 2000,
+            fill: "forwards",
+        }
+    );
+    setTimeout(() => removeFromMain(pageToRemove), 2000);
 }
 
 function enterMain(page) {
-  mainBody.appendChild(page);
-  page.animate(
-    [
-      { transform: "translate(-50vw, 0)", opacity: 0 },
-      { transform: "translate(0, 0)", opacity: 1 },
-    ],
-    {
-      duration: 2000,
-      fill: "forwards",
-    }
-  );
+    mainBody.appendChild(page);
+    page.animate(
+        [
+            { transform: "translate(-50vw, 0)", opacity: 0 },
+            { transform: "translate(0, 0)", opacity: 1 },
+        ],
+        {
+            duration: 2000,
+            fill: "forwards",
+        }
+    );
 }
