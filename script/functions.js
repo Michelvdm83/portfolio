60272483ac5dff345fc240e247ae06651e0c010f
Michelvdm83 <140594903+Michelvdm83@users.noreply.github.com>
2025-04-15 10:06:46 +0200

    Update functions.js

--- script/functions.js
+++ script/functions.js
@@ -3,7 +3,7 @@ const sora2Div = document.getElementById("sora2");
 const movers = document.getElementById("carousel");
 
 function firstLoad() {
-    fetch("https://animepahe.ru/", {mode: "no-cors"})
+    fetch("https://animepahe.ru/api?m=search&q=%22my%20hero%20academia%22")
         .then((response) => console.log(response));
     movers.childNodes.forEach((navButton) => {
         let newNode = navButton.cloneNode(true);
